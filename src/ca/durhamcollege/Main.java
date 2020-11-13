8a4dea8dd5c8cee253725cc1c36fb6d6c5be5cee
DC-RSingh <raje.singh@dcmail.ca>
2020-11-12 22:36:55 -0500

    implement multi-dimensional array example

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -48,11 +48,38 @@ public class Main
                 isValid = false;
             }
         }
+
         System.out.println();
 
         // Output
         System.out.printf("You Entered: %s \n", name);
         System.out.printf("You Entered: %.2f \n", age);
+
+        // Multi-dimensional Array
+
+        System.out.println("Multi-Dimensional Array Example");
+
+        int rows = 8;
+        int cols = 8;
+
+        float [][] board = new float[rows][cols];
+
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < cols; y++)
+            {
+                board[x][y] = (float)(Math.random() * 100.0) + 1.0f;
+            }
+        }
+
+        System.out.println("Showing 10 Random Values: ");
+        for (int i = 0; i < 10; i++)
+        {
+            var randRows = (int) Math.floor(Math.random() * rows);
+            var randCols = (int) Math.floor(Math.random() * cols);
+            System.out.print("row: " + randRows + " col: " + randCols + " | ");
+            System.out.printf("The Random number is: %.2f\n", board[randRows][randCols]);
+        }
 
     }
 }
